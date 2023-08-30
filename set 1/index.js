610aa544279766a51cd46a35a50e73b5cebdca66
Sairam-50 <sairams.shinrai74@gmail.com>
2023-08-30 22:34:04 +0530

    The first assignment set

---
+++ set 1/index.js
@@ -0,0 +1,44 @@
+console.log("Sairam");
+
+console.log(45+78);
+console.log(45-78);
+console.log(45*78);
+console.log(4590/78);
+
+let a=332, b=434, sum;
+sum=a+b;
+console.log(sum);
+let difference=a-b;
+let product=a*b;
+let quotient=a/b;
+let city;
+city="Tokyo";
+console.log(city);
+
+
+function sayHello(name){
+    console.log("Hello " + name);
+} 
+
+sayHello("sasuke");
+sayHello("goku");
+sayHello("kageyama");
+
+function subtractor(a,b){
+    let diff=a-b;
+    return diff;
+}
+
+console.log(subtractor(5,7));
+console.log(subtractor(567,734));
+console.log(subtractor(445,437));
+
+const dateOfBirth="feb 15 2003";
+ const favFoods=["sweets", "puliogare", "alooparatha"];
+
+ const book={
+    title: "The Alchemist",
+    author:"Paule Coelho",
+    publication:1988
+
+ };
